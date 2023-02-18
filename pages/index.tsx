37e33a3637eb3c069b377675c3dcdb15bb1d244d
Blaine <blaine.d.oneill@gmail.com>
2023-02-17 16:54:27 -0800

    update type sizing

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,19 +15,6 @@ const Signature = ({ signature }) => {
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="x-apple-disable-message-reformatting">
-    <style>
-      #body .desktop-view {
-        display: none;
-      }
-      @media screen and (min-width: 600px) {
-        #body .desktop-view {
-          display: block;
-        }
-        #body .mobile-view {
-          display: none;
-        }
-      }
-    </style>
     <title></title>
     </head>
     <body id="body" style="-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;">${html}</body></html>`
@@ -41,27 +28,14 @@ const Signature = ({ signature }) => {
 
   return (
   <>
-  <style jsx global>{`
-    .desktop-view {
-      display: none;
-    }
-    @media screen and (min-width: 600px) {
-      .desktop-view {
-        display: block;
-      }
-      .mobile-view {
-        display: none;
-      }
-    }
-  `}</style>
-  <div ref={signatureHTML} style={{ margin: 30, padding: 20, border: '1px solid #e1e1e1', fontFamily: '"Gill Sans", "Gill Sans MT", Helvetica, Arial, sans-serif' }}>
+  <div ref={signatureHTML} style={{ padding: 0, fontFamily: '"Gill Sans", "Gill Sans MT", Helvetica, Arial, sans-serif' }}>
     <div className='desktop-view'>
     <table cellPadding={0} cellSpacing={0}>
       <tbody>
         <tr>
-          <td style={{verticalAlign: 'top'}}>
-            <span style={{fontWeight: 600, fontSize: '11.5px', lineHeight: '10px'}}>{signature.name}</span><br /><br />
-            <div className='meta' style={{fontWeight: 400, fontSize: '8.5px', lineHeight: '11.5px'}}>
+          <td style={{verticalAlign: 'top', fontSize: '7px', lineHeight: '7px', paddingTop: '8px'}}>
+            <span style={{fontWeight: 600, fontSize: '8px', lineHeight: '7px'}}>{signature.name}</span><br /><br /><br /><br />
+            <div className='meta' style={{fontWeight: 400, fontSize: '7px', lineHeight: '8px'}}>
               <span>{signature.role}</span>
               {signature.phone && (
               <>
@@ -69,85 +43,46 @@ const Signature = ({ signature }) => {
                 <span>{signature.phone_label} <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:${signature.phone.replace(/\s/g, '')}`}>{signature.phone}</a></span>
               </>
               )}
+              {signature.insta && signature.insta_link && (
+                <>
+                  <br />
+                  <span><a style={{color: '#000', textDecoration: 'unset'}} href={signature.insta_link} target="_blank"><img style={{marginBottom: '-2px'}} alt="Instagram" height="6" src="https://github.com/beelaineo/pb-signatures/blob/main/assets/insta.png?raw=true" /> {signature.insta}</a></span>
+                </>
+              )}
               <br />
-              <a style={{color: '#000', lineHeight: '10px', textDecoration: 'unset'}} href="https://www.photobombproduction.com/" target="_blank">photobombproduction.com</a>
+              <a style={{color: '#000', lineHeight: '8px', textDecoration: 'unset'}} href="https://www.photobombproduction.com/" target="_blank">photobombproduction.com</a>
             </div>
           </td>
           <td style={{verticalAlign: 'top'}}>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px', borderLeft: '0.5px solid #000', marginLeft: 16, paddingLeft: 16}}>
+            <div style={{fontWeight: 300, fontSize: '6px', lineHeight: '8px', borderLeft: '0.5px solid #000', marginLeft: 10, paddingLeft: 10, paddingTop: 4}}>
               <span style={{fontWeight: 600, color: '#000', textDecoration: 'unset'}}>Photobomb Production NYC</span><br />
               <span>360</span> <span>W</span> <span>34</span> <span>St</span> <span>Suite 8R</span><br />
               <span>New</span> <span>York,</span> <span>NY</span> <span>10001</span><br />
               Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+16464775559`}>+1 646 477 5559</a><br /><br /><br />
+              {signature.insta && signature.insta_link && (<br />)}
               <div style={{marginBottom: '5px'}} className="insta-link">
-                <img style={{marginBottom: '-2px'}} alt="Instagram" height="8" src="https://github.com/beelaineo/pb-signatures/blob/main/assets/insta.png?raw=true" />
+                <img style={{marginBottom: '-2px'}} alt="Instagram" height="6" src="https://github.com/beelaineo/pb-signatures/blob/main/assets/insta.png?raw=true" />
                 <a style={{color: '#000', textDecoration: 'unset', paddingLeft: 2}} href="https://instagram.com/photobombproduction" target="_blank">photobombproduction</a>
               </div>
             </div>
           </td>
           <td style={{verticalAlign: 'top'}}>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px', paddingLeft: 16}}>
+            <div style={{fontWeight: 300, fontSize: '6px', lineHeight: '8px', paddingLeft: 10, paddingTop: 4}}>
               <span style={{fontWeight: 600, color: '#000', textDecoration: 'unset'}}>Photobomb Production LA</span><br />
               <span>12030</span> <span>Viewcrest</span> <span>Road</span><br />
               <span>Studio</span> <span>City,</span> <span>CA</span> <span>91604</span><br />
               Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+13235405700`}>+1 323 540 5700</a><br /><br />
+              {signature.insta && signature.insta_link && (<br />)}
               <span style={{fontWeight: 600}}>Invoicing</span><br />
               <a style={{color: '#000', textDecoration: 'unset'}} href="mailto:invoices@photobombproduction.com" target="_blank">invoices@photobombproduction.com</a>
             </div>
           </td>
           <td style={{verticalAlign: 'top'}}>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px', paddingLeft: 0}}>
+            <div style={{fontWeight: 300, fontSize: '6px', lineHeight: '8px', paddingLeft: 10, paddingTop: 4}}>
               <span style={{fontWeight: 600, color: '#000', textDecoration: 'unset'}}>Photobomb Production LDN</span><br />
               <span>15</span> <span>Poland</span> <span>St</span><br />
               <span>London,</span> <span>WIF 8QE</span><br />
               Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+442079936051`}>+44 20 7993 6051</a>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
-    <div className='mobile-view'>
-    <table cellPadding={0} cellSpacing={0}>
-      <tbody>
-        <tr>
-          <td style={{paddingRight: 16, verticalAlign: 'top'}}>
-            <span style={{fontWeight: 600, fontSize: '11.5px', lineHeight: '11px'}}>{signature.name}</span><br />
-            <div className='meta' style={{paddingTop: '12px', fontWeight: 400, fontSize: '8.5px', lineHeight: '12px'}}>
-              <span>{signature.role}</span>
-              {signature.phone && (
-              <>
-                <br />
-                <span>{signature.phone_label} <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:${signature.phone.replace(/\s/g, '')}`}>{signature.phone}</a></span>
-              </>
-              )}<br />
-              <a style={{color: '#000', lineHeight: '10px', textDecoration: 'unset'}} href="https://www.photobombproduction.com/" target="_blank">photobombproduction.com</a><br /><br /><br /><br />
-              <div style={{marginBottom: '5px'}} className="insta-link">
-                <img style={{marginBottom: '-2px'}} alt="Instagram" height="8" src="https://github.com/beelaineo/pb-signatures/blob/main/assets/insta.png?raw=true" />
-                <a style={{color: '#000', textDecoration: 'unset', paddingLeft: 2, fontWeight: 300}} href="https://instagram.com/photobombproduction" target="_blank">photobombproduction</a>
-              </div>
-              <span style={{fontWeight: 600}}>Invoicing</span><br />
-              <a style={{color: '#000', textDecoration: 'unset', fontWeight: 300}} href="mailto:invoices@photobombproduction.com" target="_blank">invoices@photobombproduction.com</a>
-            </div>
-          </td>
-          <td style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px', borderLeft: '0.5px solid #000', paddingTop: 5, paddingLeft: 16, verticalAlign: 'top'}}>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px'}}>
-              <span style={{fontWeight: 600}}>Photobomb Production NYC</span><br />
-              <span>360</span> <span>W</span> <span>34</span> <span>St</span> <span>Suite 8R</span><br />
-              <span>New</span> <span>York,</span> <span>NY</span> <span>10001</span><br />
-              Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+16464775559`}>+1 646 477 5559</a><br /><span style={{lineHeight: '12px'}}><br /></span>
-            </div>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px'}}>
-              <span style={{fontWeight: 600}}><span>Photobomb</span> <span>Production</span> <span>L</span><span>A</span></span><br />
-              <span>12030</span> <span>Viewcrest</span> Road<br />
-              <span>Studio</span> <span>City,</span> <span>CA</span> <span>91604</span><br />
-              Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+13235405700`}>+1 323 540 5700</a><br /><span style={{lineHeight: '12px'}}><br /></span>
-            </div>
-            <div style={{fontWeight: 300, fontSize: '7.5px', lineHeight: '10px', paddingBottom: 5}}>
-              <span style={{fontWeight: 600}}>Photobomb Production LDN</span><br />
-              <span>15</span> <span>Poland</span> St<br />
-              <span>London,</span> <span>WIF</span> <span>8QE</span><br />
-              Office: <a style={{color: '#000', textDecoration: 'unset'}} href={`tel:+442079936051`}>+44 20 7993 6051</a><br />
             </div>
           </td>
         </tr>
